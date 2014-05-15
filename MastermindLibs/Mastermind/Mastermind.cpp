481cb5378d0c2549926ddce67e26617c28193664
Anthony <tony.vallee@hotmail.com>
2014-05-15 15:45:32 -0400

    MasterMind class update
    Le constructeur et le destructeur de la classe MasterMind ont été faits.

--- MastermindLibs/Mastermind/Mastermind.cpp
+++ MastermindLibs/Mastermind/Mastermind.cpp
@@ -4,10 +4,38 @@
 Mastermind::Mastermind()
 {
    //Il faut créer ici les 4096 séquences de couleurs possibles. Bonne réflexion!
+	const unsigned short NOMBRE_COULEURS = 4;
+	int position = 0;
+	//Initialiser la liste
+	list = new List<ArrayI<Color>>();
+
+	for (int premierIncre = 1; premierIncre < 9; premierIncre++)
+	{
+		for (int deuxiemeIncre = 1; deuxiemeIncre < 9; deuxiemeIncre++)
+		{
+			for (int troisiemeIncre = 1; deuxiemeIncre < 9; troisiemeIncre++)
+			{
+				for (int quatriemeIncre = 1; quatriemeIncre < 9; quatriemeIncre++)
+				{
+					tabSequences[position] = new ArrayI<Color>(NOMBRE_COULEURS);
+					tabSequences[position]->SetElement(premierIncre, 0);
+					tabSequences[position]->SetElement(deuxiemeIncre, 1);
+					tabSequences[position]->SetElement(troisiemeIncre, 2);
+					tabSequences[position]->SetElement(quatriemeIncre, 3);
+					list->PushBack(tabSequences[position]);
+					position++;
+				}
+			}
+		}
+	}
 }
 
 Mastermind::~Mastermind()
 {
+	for (int i = 0; i < 4096; i++)
+	{
+		delete tabSequences[i];
+	}
 }
 
 int Mastermind::GetNbElements() const
